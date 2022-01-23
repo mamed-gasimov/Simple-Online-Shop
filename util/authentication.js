87a6b8aadb6155b3ca5c8d80a1dd244a3a1452cb
Mammadali Gasimov <mamed-gasimov@Mamed.local>
2022-01-23 18:10:54 +0400

    added nav bar, basic styles for it, and admin nav items

--- util/authentication.js
+++ util/authentication.js
@@ -1,5 +1,6 @@
 const createUserSession = (req, user, action) => {
     req.session.uid = user._id.toString();
+    req.session.isAdmin = user.isAdmin;
     req.session.save(action); 
     // save method from express-session package, it will execute action when session saving on db will be done
 }
