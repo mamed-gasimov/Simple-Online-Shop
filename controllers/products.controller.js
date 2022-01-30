c6a01da98be0a0c025d718522590959c01454544
Mammadali Gasimov <mamed-gasimov@Mamed.local>
2022-01-30 17:38:21 +0400

    outputting all products for customers

---
+++ controllers/products.controller.js
@@ -0,0 +1,15 @@
+const Product = require('../models/product.model');
+
+const getAllProducts = async (req, res, next) => {
+    try {
+        const products = await Product.findAll();
+        res.render('customer/products/all-products', { products });
+    } catch (error) {
+        next(error);
+        return;
+    }
+}
+
+module.exports = {
+    getAllProducts,
+}
